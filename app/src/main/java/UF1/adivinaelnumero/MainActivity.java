a1c7c81bdc37de58a0657e229bcce4b0d7e0dbed
itsJRillo <jmorillolabour@gmail.com>
2022-09-16 19:19:53 +0200

    Aplicación acabada

--- app/src/main/java/UF1/adivinaelnumero/MainActivity.java
+++ app/src/main/java/UF1/adivinaelnumero/MainActivity.java
@@ -4,19 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int rand = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int rand = 10;
+        final EditText number = findViewById(R.id.intento);
+        final Button adivinar = findViewById(R.id.adivinar);
+
+        adivinar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(Integer.parseInt(number.getText().toString()) == rand){
+                    GoodToast();
+                } else {
+                    BadToast();
+                }
+            }
+        });
     }
 
-    public void Toast(CharSequence text){
+    public void BadToast(){
         Context context = getApplicationContext();
+        CharSequence text = "¡¡ Wrong number !!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void GoodToast(){
+        Context context = getApplicationContext();
+        CharSequence text = "¡¡ Congratulations !!";
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);
