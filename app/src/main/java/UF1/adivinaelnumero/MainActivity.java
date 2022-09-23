81e7add6c869443fe110fc83e87868872840d3ca
itsJRillo <jmorillolabour@gmail.com>
2022-09-23 18:55:05 +0200

    Registros del juego

--- app/src/main/java/UF1/adivinaelnumero/MainActivity.java
+++ app/src/main/java/UF1/adivinaelnumero/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,24 +17,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int rand = 10;
+        int rand = (int) Math.floor(Math.random() * 100);
+        final int[] intentos = {0};
+        final TextView registros = findViewById(R.id.mensaje);
         final EditText number = findViewById(R.id.intento);
         final Button adivinar = findViewById(R.id.adivinar);
 
+
         adivinar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if(Integer.parseInt(number.getText().toString()) > rand){
-                    CharSequence text = "Es más pequeño";
-                    Toast(text);
-                } else if(Integer.parseInt(number.getText().toString()) < rand){
-                    CharSequence text = "Es más grande";
-                    Toast(text);
+                registros.append("\nIntentos restantes: " + String.valueOf(5 - intentos[0]) + "\n");
+                intentos[0] += 1;
+                if(Integer.parseInt(number.getText().toString()) < rand){
+                    registros.append("Es más grande\n");
+                } else if (Integer.parseInt(number.getText().toString()) > rand){
+                    registros.append("Es más pequeño\n");
                 } else {
-                    CharSequence text = "¡¡Congratulations!!";
-                    Toast(text);
+                    registros.append("\nCongratulations\nNúmero a adivinar: " + rand);
+                    adivinar.setEnabled(false);
                 }
+
+                if (intentos[0] == 5){adivinar.setEnabled(false);}
+
             }
         });
+
+        registros.setText("");
     }
 
     public void Toast(CharSequence text){
