abcbf821b7f55418821783cff70b90ceed93b614
itsJRillo <jmorillolabour@gmail.com>
2022-09-30 19:14:38 +0200

    RecordActivity acabado

--- app/src/main/java/UF1/adivinaelnumero/MainActivity.java
+++ app/src/main/java/UF1/adivinaelnumero/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
@@ -16,19 +17,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
     int intentos = 0;
+    int totalIntentos = 0;
     int rand = (int) Math.floor(Math.random() * 100);
+    private String m_text = "";
+    String jugador = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog.Builder record = new AlertDialog.Builder(this);
+
         final TextView registros = findViewById(R.id.mensaje);
         registros.setMovementMethod(new ScrollingMovementMethod());
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         final EditText number = findViewById(R.id.intento);
         final Button adivinar = findViewById(R.id.adivinar);
@@ -61,6 +69,26 @@ public class MainActivity extends AppCompatActivity {
                     Toast(text);
                     registros.append("Es más pequeño\n");
                 } else if(Integer.parseInt(number.getText().toString()) == rand){
+                    totalIntentos = intentos;
+
+                    final EditText input = new EditText(MainActivity.this);
+                    input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    record.setMessage("Introduzca su nombre: ");
+                    record.setView(input);
+                    record.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            m_text = input.getText().toString();
+                            jugador = m_text;
+                        }
+                    });
+                    record.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    AlertDialog recordUser = record.create();
 
                     builder.setMessage("¡¡¡ Congratulations !!!");
                     builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
@@ -70,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
                             intentos = 0;
                             registros.setText("Aquí aparecerán los resultados\n");
                             number.setText("");
+                            recordUser.show();
                         }
                     });
 
@@ -107,6 +136,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view) {
         Intent intent = new Intent(this, WinnersActivity.class);
+        intent.putExtra("nombre_player",jugador);
+        intent.putExtra("intentos_player",totalIntentos);
         startActivity(intent);
     }
 
--- app/src/main/java/UF1/adivinaelnumero/WinnersActivity.java
+++ app/src/main/java/UF1/adivinaelnumero/WinnersActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class WinnersActivity extends AppCompatActivity {
 
@@ -15,6 +16,18 @@ public class WinnersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_winners);
 
         final Button play = findViewById(R.id.play);
+        Bundle extras = getIntent().getExtras();
+        String repeated = new String(new char[45]).replace("\0", " ");
+
+        if (extras != null) {
+            final TextView playerName = findViewById(R.id.dataPlayers);
+
+            String name = extras.getString("nombre_player");
+            int tries = extras.getInt("intentos_player");
+
+            if(name == null && tries == 0){return;}
+            playerName.append(name + repeated + tries + "\n");
+        }
 
         play.setOnClickListener(new View.OnClickListener() {
             @Override
